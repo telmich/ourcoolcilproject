063ee597ca7e9870a10eaf041b07d1bdf1670759
Nico Schottelius <nico@feder.schottelius.org>
2017-03-31 08:42:18 +0200

    + stuff for ex5

--- nico.py
+++ nico.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 # From exercise 5
@@ -42,35 +44,76 @@ def partial(x, row, column, U, Zt):
     return a + b
 
 
-def ex5_load(dirname):
-    image_dir = "CroppedYale/"
+def quadratic_cost_frobenius_J(X, U, V):
+    return (1/2) * (np.linalg.norm(X - (U.T * V)) )**2
 
-    files = os.listdir(image_dir)
-    n = len(files)
-    print("Loading " + str(n) + " images")
 
-    imgs = [Image.open(image_dir + files[i]) for i in range(n)]
+def project_non_negative(matrix):
+    """Compare to CIL2017-04-Non-Negative.pdf page 30"""
+
+    for x in np.nditer(matrix, op_flags=['readwrite']):
+        x[...] = max(0, x)
+
+
+def random_non_negative_matrix(rows, columns, max_val=255):
+    random.seed()
+
+    matrix = np.empty([rows, columns])
+
+    for x in np.nditer(matrix, op_flags=['readwrite']):
+        x[...] = random.randint(0, max_val)
+
+
+
+def ex5_load(image_dir):
+    files = [ os.path.join(image_dir, image) for image in os.listdir(image_dir) ]
+
+    print("Loading {} images".format(len(files)))
+
+    imgs = [Image.open(image) for image in files]
+
+    plt.show(imgs[0])
+
     # Assume all images have the same size
-    img0 = imgs[0]width, height = img0.size
+    img0 = imgs[0]
+    width, height = img0.size
 
-    # Compute input matrix X
-    X_list = [np.ravel(imgs[i].getdata()) for i in range(n)]
+    # Compute input matrix X: one vector per image
+    X_list = [np.ravel(imgs[i].getdata()) for i in
+              range(len(files))]
+
+    # Convert it to numpy input
     X = np.array(X_list, dtype=np.float32)
+
     print(X.shape)
 
+    return (X, width)
 
-def ex5_show(V):
+def ex5_non_negative():
+    pass
+
+def ex5_show(V, width):
     new_images_stacked = np.reshape(V, (-1, width))
     fig1 = plt.figure()
-    plt.imshow(new_images_stacked)
+    plt.show(new_images_stacked)
 
+
+def ex5(image_dir):
+    X, width = ex5_load(image_dir)
+
+    # X_still_positive = project_non_negative(X)
+
+    # ex5_show(X, width)
 
 if __name__ == '__main__':
+    # EX 4 test code
+    # U = np.ones([3,4])
+    # Zt = np.ones([4,2])
 
-    U = np.ones([3,4])
-    Zt = np.ones([4,2])
+    # print(U.shape)
+    # print(Zt.shape)
 
-    print(U.shape)
-    print(Zt.shape)
+    # print("Res: {}".format(  partial(3, 1, 1, U, Zt)))
 
-    print("Res: {}".format(  partial(3, 1, 1, U, Zt)))
+    # EX 5
+    ex5("/home/users/nico/eth/cil/lecture_cil_public/exercises/ex5/CroppedYale/CroppedYale")
