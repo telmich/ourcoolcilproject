4154b10fb38ccb7b8e4a8a2741fd1983870787b3
Nico Schottelius <nico@nico-notebook.schottelius.org>
2018-02-11 21:40:21 +0100

    Begin to cleanup from last year

---
+++ ex9.py
@@ -0,0 +1,11 @@
+
+class CNN(object):
+    def __init__(self, imagesize):
+        self.size = imagesize
+
+    def genimage(channel):
+        pass
+
+    def filter():
+        """How to filter over
+        pass
